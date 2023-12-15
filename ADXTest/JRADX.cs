aed591f0aa7ec8772271be9f72b6e6e4e7754bd5
Jonathan Robertson <jonathan.robertson@aquaticinformatics.com>
2023-12-15 16:15:53 -0700

    push to adx using direct ingestion client and prompting for auth

---
+++ ADXTest/JRADX.cs
@@ -0,0 +1,78 @@
+﻿using Kusto.Data;
+using Kusto.Data.Common;
+using Kusto.Ingest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADXTest
+{
+    internal class JRADX
+    {
+        public async Task DoStuff()
+        {
+            JREvent model = new JREvent
+            {
+                ID = 1,
+                Name = "Test"
+            };
+
+            string json = JsonConvert.SerializeObject(model);
+
+            await Ingest(json, "redacted", "JR", "JREvents");
+        }
+
+
+        public async Task Ingest(string jsonPayload, string ingestUri, string databaseName, string tableName)
+        {
+            // WithAadUserPromptAuthentication() prompts to login...
+            // easy for proof of concept
+            KustoConnectionStringBuilder kustoConnectionStringBuilder = new KustoConnectionStringBuilder(ingestUri).WithAadUserPromptAuthentication();
+
+            byte[] payloadBytes = Encoding.ASCII.GetBytes(jsonPayload);
+            MemoryStream byteStream = new MemoryStream(payloadBytes);
+
+            StreamSourceOptions sourceOptions = new StreamSourceOptions
+            {
+                Size = payloadBytes.Length,
+                SourceId = Guid.NewGuid()
+            };
+
+
+            KustoIngestionProperties props = new KustoIngestionProperties
+            {
+                DatabaseName = databaseName,
+                TableName = tableName,
+                IngestionMapping = new IngestionMapping
+                {
+                    IngestionMappingReference = ""
+                },
+                Format = DataSourceFormat.multijson
+            };
+            props.AdditionalProperties.Add("OperationTimeoutMs", "60000");
+
+
+            try
+            {
+                // should use one client per request
+                //using (var client = KustoIngestFactory.CreateQueuedIngestClient(kustoConnectionStringBuilder))
+                //{
+                //    await client.IngestFromStreamAsync(byteStream, props, sourceOptions);
+                //}
+
+                using (var client = KustoIngestFactory.CreateDirectIngestClient(kustoConnectionStringBuilder))
+                {
+                    var result = await client.IngestFromStreamAsync(byteStream, props, sourceOptions);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // catching exception for debugging
+            }
+        }
+    }
+}
---
+++ ADXTest/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ADXTest;
+
+Console.WriteLine("Hello, World!");
+
+JRADX jRADX = new JRADX();
+await jRADX.DoStuff();
